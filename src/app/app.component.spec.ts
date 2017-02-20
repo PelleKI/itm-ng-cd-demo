22e43218f68dd2921aa8bca306e4f191d37c03b1
Bjørn <bjoern.soerensen@gmail.com>
2017-02-20 23:30:05 +0100

    Fixed e2e test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,13 +20,13 @@ describe('AppComponent', () => {
   it(`should have as title 'Angular demo application for Continuous Delivery demo'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Angular demo application for Continuous Delivery demo');
+    expect(app.title).toEqual('Angular demo application for Continuous Delivery demo v2');
   }));
 
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Angular demo application for Continuous Delivery demo');
+    expect(compiled.querySelector('h1').textContent).toContain('Angular demo application for Continuous Delivery demo v2');
   }));
 });
