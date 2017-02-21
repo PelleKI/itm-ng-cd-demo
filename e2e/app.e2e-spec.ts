8ae3fd8d46ae01d2cda974e0c4a992c5f9b7c22d
Bjørn <bjoern.soerensen@gmail.com>
2017-02-21 16:10:11 +0100

    Revert v2

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('demo App', () => {
 
   it('should display message saying app works', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Angular demo application for Continuous Delivery demo v2 dev');
+    expect(page.getParagraphText()).toEqual('Angular demo application for Continuous Delivery demo');
   });
 });
